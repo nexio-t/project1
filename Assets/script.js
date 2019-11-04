842a70a6a286f5c087aad5f4a969e27d09a60de2
Moshe Stern <moshecstern@gmail.com>
2019-11-04 10:52:33 -0500

    yelp api further work

--- Assets/script.js
+++ Assets/script.js
@@ -14,6 +14,25 @@ $(document).ready(function () {
   // vars for the divs on page
   var price = "paid";
 
+  // ********************** EVENTBRITE API *********************************
+  // calling function to show eventbrite api search
+  $(document).on("click", "#event-brite", function (event) {
+    event.preventDefault();
+    displayapieventbrite();
+  });
+  // on botton click change search results to free or paid only
+  $("#priceBtn").on("click", function (event) {
+    event.preventDefault();
+    if (price == "paid") {
+      price = "free";
+      console.log(price);
+      $("#priceBtn").css("background-color", "green");
+    } else {
+      price = "paid"
+      console.log(price);
+      $("#priceBtn").css("background-color", "red");
+    }
+  }) // end of free button click
 
   function displayapieventbrite() {
 
@@ -47,7 +66,9 @@ $(document).ready(function () {
       }
     }).then(function (response) {
       console.log("eventbright RESPONSE");
+
       var REALeventBrightResultsDiv = $("<div class='resultsDiv'>");
+
       for (i = 0; i < response.events.length; i++) {
         console.log(response);
         // console.log(response.events);
@@ -77,7 +98,7 @@ $(document).ready(function () {
         eventBrightResultsDiv.append(newRowEB2);
 
         // add link to event    response.events[i].resource_uri      or response.events[i].url
-
+        console.log("link is " + response.events[i].url);
         // add costs   cant find
 
         // add fav button
@@ -85,12 +106,14 @@ $(document).ready(function () {
         REALeventBrightResultsDiv.append(eventBrightResultsDiv);
 
       } // end of loop
-      $("#eventResultsDiv").html(REALeventBrightResultsDiv);
+      $("#eventResultsDiv").append(REALeventBrightResultsDiv);
 
     });// end of event bright api search
   }; // end of display eventbrite
 
-  // start of weather api 
+
+  // ************************* WEATHER API ***********************************
+
   function displayweather() {
     var weatherAPI = "880db94beca7f4a9dc073f7b0320f24d";
     // clear weather results div
@@ -109,14 +132,15 @@ $(document).ready(function () {
         console.log(response);
         console.log("city name " + response.city.name)
 
-        // div to hold overarching weather data
-        var rowForWeatherResults = $("<div class='resultsDiv'>");
-
         var weatherCityName = response.city.name;
-        var NWRow1 = $("<div>").text(weatherCityName);
-        rowForWeatherResults.append(NWRow1);
+        var NWRow1 = $("<div>").text("Weather Results for " + weatherCityName);
+        $("#weatherResultsDiv").prepend(NWRow1);
 
         for (i = 0; i < response.list.length; i += 3) {
+          // div to hold overarching weather data
+          var rowForWeatherResults = $("<div class='resultsDiv'>");
+
+
           console.log(response.list[i]);
 
           // date & time
@@ -157,19 +181,7 @@ $(document).ready(function () {
       }); // end of weather api search
   }; // end of displayweather function
 
-  // on botton click change search results to free or paid only
-  $("#priceBtn").on("click", function (event) {
-    event.preventDefault();
-    if (price == "paid") {
-      price = "free";
-      console.log(price);
-      $("#priceBtn").css("background-color", "green");
-    } else {
-      price = "paid"
-      console.log(price);
-      $("#priceBtn").css("background-color", "red");
-    }
-  }) // end of free button click
+// ************************** TICKET MASTER API ********************************
 
   // on btn click to display sports events in event section
   $("#ticket-master").on("click", function (event) {
@@ -186,7 +198,7 @@ $(document).ready(function () {
       console.log(response + " my response is");
       console.log(response._embedded);
       console.log(response._embedded.events);
-      var REALticketMasterResults = $("<div>");
+      var REALticketMasterResults = $("<div class='resultsDiv'>");
       for (i = 0; i < response._embedded.events.length; i++) {
         console.log(response._embedded.events[i].name);
         var ticketMasterResults = $("<div class='resultsDiv'>");
@@ -200,17 +212,17 @@ $(document).ready(function () {
         ticketMasterResults.append(newTD1);
 
         // but the ticket url
-        var ticketmasterURL = response._embedded.events[i].url; 
+        var ticketmasterURL = response._embedded.events[i].url;
         var newTD2 = $("<div>");
-        $(newTD2).append('<a class="yelpLink" href='+ ticketmasterURL +'target="_blank"' +'>click me for tickets!;</a>');
+        $(newTD2).append('<a class="yelpLink" href=' + ticketmasterURL + 'target="_blank"' + '>click me for tickets!;</a>');
         ticketMasterResults.append(newTD2);
 
-      
+
 
         // event poster  response._embedded.events[i].images[3]
         var ticketmasterPoster = response._embedded.events[i].images[3].url;
         var newTD3 = $("<div>");
-        newTD3.append('<img class= "theImg" src="'+ticketmasterPoster+'"/>');
+        newTD3.append('<img class= "theImg" src="' + ticketmasterPoster + '"/>');
         //not working proper
         $(".theImg").css("width", "150px");
         $(".theImg").css("height", "150px");
@@ -223,32 +235,23 @@ $(document).ready(function () {
         console.log("venue name " + response._embedded.events[i]._embedded.venues[0].name);
 
 
-
-
         // add to event results div
         $(REALticketMasterResults).append(ticketMasterResults);
       }// end of loop
-      $("#eventResultsDiv").html(REALticketMasterResults);
-
-
-
+      $("#eventResultsDiv").append(REALticketMasterResults);
 
     });// end of ticket master api search
   }); // end of on ticket-master events click
 
-  // calling function to show api search
-  $(document).on("click", "#event-brite", function (event) {
-    event.preventDefault();
-    displayapieventbrite();
-  });
 
-  $(document).on("click", "#submit", function (event) {
+  //***************************** yelp business api ***********************************
+
+  $(document).on("click", "#business", function (event) {
     event.preventDefault();
     displayYelpApi();
     displayweather();
   });
 
-  //yelp api
   function displayYelpApi() {
     // add function for displaying news results
     // on main submit button display weather results
@@ -287,103 +290,188 @@ $(document).ready(function () {
         console.log("poster image url " + business[i].image_url);
         var yelpPoster = business[i].image_url;
         var yelpRow2 = $("<div>")
-        yelpRow2.append('<img class="theImg" src="'+yelpPoster+'"/>');
+        yelpRow2.append('<img class="theImg" src="' + yelpPoster + '"/>');
         $(".theImg").css("width", "150px");
         $(".theImg").css("height", "150px");
-        
+
         yelpResultsDiv.append(yelpRow2);
-        
+
         // business[i].url   for  events own website
         console.log("url " + business[i].url);
         var yelpURL = business[i].url;
         var yelpRow3 = $("<div>");
-        $(yelpRow3).append('<a class="yelpLink" href='+ yelpURL +'target="_blank"' +'>click me!;</a>');
+        $(yelpRow3).append('<a class="yelpLink" href=' + yelpURL + 'target="_blank"' + '>click me!;</a>');
         yelpResultsDiv.append(yelpRow3);
-        
+
         // price range   business[i].price
         console.log("price range " + business[i].price);
         var yelpPrice = business[i].price;
         var yelpRow4 = $("<div>").text(yelpPrice);
         yelpResultsDiv.append(yelpRow4);
-        
+
         // location business[i].location.display_address[0] + business[i].location.display_address[1] ;
         console.log("location1 " + business[i].location.display_address[0]);
         console.log("location2 " + business[i].location.display_address[1]);
         var yelpLocation = business[i].location.display_address[0] + ", " + business[i].location.display_address[1];
         var yelpRow5 = $("<div>").text(yelpLocation);
         yelpResultsDiv.append(yelpRow5);
-        
+
         $("#eventResultsDiv").append(yelpResultsDiv);
       } // end of loop
-      
+
     });
   };// end of yelp api search
-  $('.yelpLink').click(function () {
-    window.location = $(this).attr('href');
-  }).click()
 
 
   //--------------------------------------Google News API Start---------------------------------------------//
 
-  $(document).on("click", "#submit", function() {
+  $(document).on("click", "#submit", function () {
 
     // Update this later 
-    var city = $("#city").val().trim(); 
+    var city = $("#city").val().trim();
 
     var noSpacesCity = city.replace(" ", "%20")
 
-    console.log("city is " + noSpacesCity); 
+    console.log("city is " + noSpacesCity);
 
     // Update the URL
-    var googleURL = "https://newsapi.org/v2/everything?q=" + noSpacesCity +"&sources=the-new-york-times,usa-today,abc-news&sortBy=relevancy&apiKey=963471a1dfbe44c6a4c1fa29c815655b";
+    var googleURL = "https://newsapi.org/v2/everything?q=" + noSpacesCity + "&sources=the-new-york-times,usa-today,abc-news&sortBy=relevancy&apiKey=963471a1dfbe44c6a4c1fa29c815655b";
 
     console.log("google news URL is " + googleURL);
 
     $.ajax({
-        url: googleURL,
-        method: "GET"
-      }).then(function(result) {
+      url: googleURL,
+      method: "GET"
+    }).then(function (result) {
 
-        console.log("google news" + result); 
+      console.log("google news" + result);
 
-        for (var i = 0; result.length; i++) {
+      for (var i = 0; result.length; i++) {
 
-           var title = ""; 
-           var description = "";
-           var content = ""; 
-           var articleURL = ""; 
+        var title = "";
+        var description = "";
+        var content = "";
+        var articleURL = "";
 
-          //  var imageArea = $("<div></div>"); 
+        //  var imageArea = $("<div></div>"); 
 
-          //  imageArea.addClass("img-div");
+        //  imageArea.addClass("img-div");
 
-          //  var imageDiv = $("<img>").attr("src", dataStill); 
+        //  var imageDiv = $("<img>").attr("src", dataStill); 
 
-          //  imageDiv.addClass("click-area"); 
+        //  imageDiv.addClass("click-area"); 
 
-          //  imageDiv.attr("data-state", "still"); 
+        //  imageDiv.attr("data-state", "still"); 
 
-          //  imageDiv.attr("data-still", dataStill);
+        //  imageDiv.attr("data-still", dataStill);
 
-          //  imageDiv.attr("data-animate", dataAnimate);
+        //  imageDiv.attr("data-animate", dataAnimate);
 
-          //  $(imageArea).append(imageDiv); 
+        //  $(imageArea).append(imageDiv); 
 
-          //  $(imageArea).append("<p>Rating: " + rating + "</p>"); 
+        //  $(imageArea).append("<p>Rating: " + rating + "</p>"); 
 
-          //  $(".gif-area").append(imageArea);
+        //  $(".gif-area").append(imageArea);
 
-           // on click for class of sports 
-           
-        };
-        
-      });
+        // on click for class of sports 
 
-});
+      };
+
+    });
+
+  });
 
 
 
   //--------------------------------------Google News API End---------------------------------------------//
+
+
+
+
+//***************************** yelp event search  api ***********************************
+
+$(document).on("click", "#submit", function (event) {
+  event.preventDefault();
+  displayYelpEventApi();
+  displayweather();
+  $("#eventResultsDiv").empty();
+});
+
+function displayYelpEventApi() {
+  // add function for displaying news results
+  // on main submit button display weather results
+
+  eventType = $("#eventType").val();
+  city = $("#city").val();
+  console.log();
+  // ajaxCall($(this).attr("data-term"));
+  var queryYelpEventURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + "events"  + "&location=" + city + "&radius=40000&limit=8";
+
+  $.ajax({
+    url: queryYelpEventURL,
+    method: "GET",
+    headers: {
+      Authorization: "Bearer CRh4dVy8vCYb_8OeBP5ACVQmlTbrk6mZXHwi8VSv9jcOtYRHNHktPbFWZD6dqfqmv9zICcen2OEQx5aEI--lPD5BhlLYpblNxIBlGyJT2aNN1ZpmzytcLGCHOfW5XXYx",
+    },
+    dataType: 'json',
+  }).then(function (response) {
+    var business = response.businesses
+
+    for (var i = 0; i < business.length; i++) {
+
+      console.log(queryYelpEventURL);
+      console.log(response);
+
+      var yelpResultsDiv = $("<div class= 'resultsDiv'>");
+      $(".resultsDiv").css("margin", "5px");
+      $(".resultsDiv").css("border-style", "double");
+
+      // name
+      console.log(business[i].name);
+      var yelpName = business[i].name
+      var yelpRow1 = $("<div>").text(yelpName);
+      yelpResultsDiv.append(yelpRow1);
+
+      // business[i].image_url   for poster for event
+      console.log("poster image url " + business[i].image_url);
+      var yelpPoster = business[i].image_url;
+      var yelpRow2 = $("<div>")
+      yelpRow2.append('<img class="theImg" src="' + yelpPoster + '"/>');
+      $(".theImg").css("width", "150px");
+      $(".theImg").css("height", "150px");
+
+      yelpResultsDiv.append(yelpRow2);
+
+      // business[i].url   for  events own website
+      console.log("url " + business[i].url);
+      var yelpURL = business[i].url;
+      var yelpRow3 = $("<div>");
+      $(yelpRow3).append('<a class="yelpLink" href=' + yelpURL + 'target="_blank"' + '>click me!;</a>');
+      yelpResultsDiv.append(yelpRow3);
+
+      // price range   business[i].price
+      console.log("price range " + business[i].price);
+      var yelpPrice = business[i].price;
+      var yelpRow4 = $("<div>").text(yelpPrice);
+      yelpResultsDiv.append(yelpRow4);
+
+      // location business[i].location.display_address[0] + business[i].location.display_address[1] ;
+      console.log("location1 " + business[i].location.display_address[0]);
+      console.log("location2 " + business[i].location.display_address[1]);
+      var yelpLocation = business[i].location.display_address[0] + ", " + business[i].location.display_address[1];
+      var yelpRow5 = $("<div>").text(yelpLocation);
+      yelpResultsDiv.append(yelpRow5);
+
+      $("#eventResultsDiv").append(yelpResultsDiv);
+    } // end of loop
+
+  });
+};// end of yelp api search
+
+
+
+
+
 
 
 }); // end of on load function
